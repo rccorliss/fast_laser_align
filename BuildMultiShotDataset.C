ad1c65ba2968540b9e24ce44e07303d00fb6fe12
Ross Corliss <rcorliss@mit.edu>
2025-07-08 16:38:33 -0400

    there are now both 'sequential' and 'random' neighbors.  x1[0] is the shot, x1[1] is the one after it.  xr[0] and xr[1] are random other shots.

--- BuildMultiShotDataset.C
+++ BuildMultiShotDataset.C
@@ -46,8 +46,8 @@ void BuildMultiShotDataset(const char* originalRootFile,  const int numShots, co
     }
 
     //create the variables for the new tree.  Here we will combine multiple entries from the original tree, so we need an additional index in the variables
-    float fposNew[4][numShots], pd1New[4][numShots], pd2New[4][numShots];
-   
+    float fposNew[4][numShots*2], pd1New[4][numShots*2], pd2New[4][numShots*2];
+
     // Create branches in the new tree
     int n=numShots;
     tree->Branch("n", &n, "n/I");
@@ -55,27 +55,45 @@ void BuildMultiShotDataset(const char* originalRootFile,  const int numShots, co
         tree->Branch(Form("fpos%d", i), &(fposNew[i][0]), Form("fpos%d[n]/F", i));
         tree->Branch(Form("pd1_%d", i), &(pd1New[i][0]), Form("pd1_%d[n]/F", i));
         tree->Branch(Form("pd2_%d", i), &(pd2New[i][0]), Form("pd2_%d[n]/F", i));
+        tree->Branch(Form("fpos_r%d", i), &(fposNew[i][numShots]), Form("fpos_r%d[n]/F", i));
+        tree->Branch(Form("pd1_r%d", i), &(pd1New[i][numShots]), Form("pd1_r%d[n]/F", i));
+        tree->Branch(Form("pd2_r%d", i), &(pd2New[i][numShots]), Form("pd2_r%d[n]/F", i));
     }
 
     //also add a distance-from-center value for network training, and other new derived variables.
-    float center[numShots];
-    float x1[numShots], y1[numShots];
-    float x2[numShots], y2[numShots];
-    float s1[numShots], s2[numShots];
-    tree->Branch("center", &center, "center[n]/F");
+    float d2[numShots*2], d3[numShots*2], dist[numShots*2];
+    float x1[numShots*2], y1[numShots*2];
+    float x2[numShots*2], y2[numShots*2];
+    float s1[numShots*2], s2[numShots*2];
+    tree->Branch("dist", &dist, "dist[n]/F");
+    tree->Branch("d2", &d2, "d2[n]/F");
+    tree->Branch("d3", &d3, "d3[n]/F");
     tree->Branch("x1", &x1, "x1[n]/F");
     tree->Branch("y1", &y1, "y1[n]/F");
     tree->Branch("x2", &x2, "x2[n]/F");
     tree->Branch("y2", &y2, "y2[n]/F");
     tree->Branch("s1", &s1, "s1[n]/F");
     tree->Branch("s2", &s2, "s2[n]/F");
+    // Add branches for the random entries
+    tree->Branch("distr", &(dist[numShots]), "distr[n]/F");
+    tree->Branch("dr2", &(d2[numShots]), "dr2[n]/F");
+    tree->Branch("dr3", &(d3[numShots]), "dr3[n]/F");
+    tree->Branch("xr1", &(x1[numShots]), "xr1[n]/F");
+    tree->Branch("yr1", &(y1[numShots]), "yr1[n]/F");
+    tree->Branch("xr2", &(x2[numShots]), "xr2[n]/F");
+    tree->Branch("yr2", &(y2[numShots]), "yr2[n]/F");
+    tree->Branch("sr1", &(s1[numShots]), "sr1[n]/F");
+    tree->Branch("sr2", &(s2[numShots]), "sr2[n]/F");
 
     Long64_t nEntries = inTree->GetEntries();
     for (int it=0;it<iterations; it++) {
         for (Long64_t entry = 0; entry < nEntries; ++entry) {
             inTree->GetEntry(entry);
             // Fill the new tree with the data from the original tree
-            for (int shot = 0; shot < numShots; ++shot) {
+            
+            for (int shot = 0; shot < numShots*2; ++shot) {
+                //for the first half we pick consecutive hits, then we draw the same number of random ones (I just didn't want to write this twice)
+                //zero is the actual shot in order.
                 s1[shot] = 0;
                 s2[shot] = 0;
                 for (int i = 0; i < 4; ++i) {
@@ -92,10 +110,15 @@ void BuildMultiShotDataset(const char* originalRootFile,  const int numShots, co
                 y2[shot]=(pd2[0]+pd2[2]-pd2[3]-pd2[1])/s2[shot];
 
                 // Calculate the distance from the center for each shot
-                center[shot] = sqrt((fpos[2]-1)*(fpos[2]-1) + (fpos[3]+3)*(fpos[3]+3));//hard coded truth.
+                d2[shot] = (fpos[2]-1);
+                d3[shot] = (fpos[3]+3);
+                dist[shot] = sqrt(d2[shot]*d2[shot] + d3[shot]*d3[shot]);
                 //printf("center[%d]=%f\n",shot,center[shot]);
-                //draw a new, random entry for the next shot
-                int rndEntry = gRandom->Integer(nEntries);
+                //draw normal entries for the first numShots, then random entries for the rest
+                int rndEntry = (entry+1)% nEntries; // This will cycle through the entries
+                if (shot>numShots){
+                    rndEntry = gRandom->Integer(nEntries);
+                }
                 inTree->GetEntry(rndEntry);
             }
             // Fill the tree with the new data for this entry
